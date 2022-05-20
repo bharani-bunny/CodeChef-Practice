fe2d4b7a8f1cce55c68e57f92a9c211b15129f82
Bharani Kudala <85745629+bharani-bunny@users.noreply.github.com>
2022-05-20 15:34:28 +0530

    Update FCTRL2.java

--- ARRAYS/FCTRL2.java
+++ ARRAYS/FCTRL2.java
@@ -29,15 +29,15 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t --> 0)
-        {
-            int n = sc.nextInt();
-            BigInteger fact = new BigInteger("1");
-            for(int i = 2; i <= n; i++)
-            fact = fact.multiply(BigInteger.valueOf(i));
-            System.out.println(fact);
-        }
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t --> 0)
+		{
+		    int n = sc.nextInt();
+		    BigInteger fact = new BigInteger("1");
+		    for(int i = 2; i <= n; i++)
+		    fact = fact.multiply(BigInteger.valueOf(i));
+		    System.out.println(fact);
+		}
 	}
 }
