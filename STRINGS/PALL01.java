1254953de41650c5d3e355084fe369dc93ba0b5f
Bharani Kudala <85745629+bharani-bunny@users.noreply.github.com>
2022-05-22 13:23:17 +0530

    Update PALL01.java

--- STRINGS/PALL01.java
+++ STRINGS/PALL01.java
@@ -40,18 +40,18 @@ class Codechef
 		int t = sc.nextInt();
 		while(t--> 0)
 		{
-            int n = sc.nextInt();
-            int temp=n,r=0,sum=0;    
-            while(n>0)
-            {    
-                r=n%10;  
-                sum=(sum*10)+r;    
-                n=n/10;    
-            }    
-            if(temp==sum)    
-                System.out.println("wins");    
-            else    
-                System.out.println("loses");    
+		    int n = sc.nextInt();
+		    int temp=n,r=0,sum=0;    
+		    while(n>0)
+		    {    
+			r=n%10;  
+			sum=(sum*10)+r;    
+			n=n/10;    
+		    }    
+		    if(temp==sum)    
+			System.out.println("wins");    
+		    else    
+			System.out.println("loses");    
 		}
 	}
 }
